6fcfe3ab2dd79eff1c1aae4def9b9ff2b6ab5063
ABLEN93 <iamme5643@gmail.com>
2021-04-01 16:01:22 +0200

    mywork

---
+++ jva.js
@@ -0,0 +1,10 @@
+const num = 1.234432;
+console.log("Ceil: " + Math.ceil(num));
+console.log("Floor: " + Math.floor(num));
+
+console.log(Math.round(3321.32321));
+console.log(Math.round(326.76));
+console.log(Math.round(76788.7));
+console.log(Math.round(-9.78));
+console.log(Math.round(43.342));
+
